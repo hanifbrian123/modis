a4c1755d80ddb9c6ecafb21d8b392bee356ffb1e
Muhammad Junaidi <emje662@gmail.com>
2025-05-23 09:57:38 +0700

    refactor: Mengubah struktur folder views guru per menu

--- app/controllers/Guru.php
+++ app/controllers/Guru.php
@@ -1,32 +1,72 @@
 <?php
 class Guru extends Controller
 {
+  // KONTROLER UNTUK MENU LAPORAN
   public function index()
   {
     $data['title'] = 'Laporan';
     $this->view('templates/header_guru', $data);
-    $this->view('guru/index', $data);
+    $this->view('guru/laporan/index', $data);
     $this->view('templates/footer');
   }
+
+  // KONTROLER UNTUK MENU PELANGGARAN
   public function pelanggaran()
   {
     $data['title'] = 'Pelanggaran';
     $this->view('templates/header_guru', $data);
-    $this->view('guru/pelanggaran', $data);
+    $this->view('guru/pelanggaran/index', $data);
     $this->view('templates/footer');
   }
+
+  public function tambah_pelanggaran()
+  {
+    $data['title'] = 'Pelanggaran';
+    $this->view('templates/header_guru', $data);
+    $this->view('guru/pelanggaran/tambah_pelanggaran', $data);
+    $this->view('templates/footer');
+  }
+
+  public function daftar_pelanggaran()
+  {
+    $data['title'] = 'Pelanggaran';
+    $this->view('templates/header_guru', $data);
+    $this->view('guru/pelanggaran/daftar_pelanggaran', $data);
+    $this->view('templates/footer');
+  }
+
+  public function edit_pelanggaran()
+  {
+    $data['title'] = 'Pelanggaran';
+    $this->view('templates/header_guru', $data);
+    $this->view('guru/pelanggaran/edit_pelanggaran', $data);
+    $this->view('templates/footer');
+  }
+
+
+  // KONTROLER UNTUK MENU PEMANGGILAN
   public function pemanggilan()
   {
     $data['title'] = 'Pemanggilan';
     $this->view('templates/header_guru', $data);
-    $this->view('guru/pemanggilan', $data);
+    $this->view('guru/pemanggilan/index', $data);
     $this->view('templates/footer');
   }
+
+  // KONTROLER UNTUK MENU KONSELING
   public function konseling()
   {
     $data['title'] = 'Konseling';
     $this->view('templates/header_guru', $data);
-    $this->view('guru/konseling', $data);
+    $this->view('guru/konseling/index', $data);
+    $this->view('templates/footer');
+  }
+
+  public function detail_pesan()
+  {
+    $data['title'] = 'Konseling';
+    $this->view('templates/header_guru', $data);
+    $this->view('guru/konseling/detail_pesan', $data);
     $this->view('templates/footer');
   }
 }
--- app/views/guru/pelanggaran.php
+++
@@ -1,5 +0,0 @@
-<main class="m-6 p-8 flex-1 bg-[#ffffff] rounded-3xl shadow-lg/25">
-  <div>
-    <h1 class="text-2xl font-bold">Daftar Pelanggaran</h1>
-  </div>
-</main>
