6daab21caddfb054def02204b157d5daaf7c642d
M Rizal Febrianto <ebiebi525@gmail.com>
2025-05-26 14:38:00 +0700

    nambahkan menu konsultasi

--- app/views/siswa/konsultasi/index.php
+++ app/views/siswa/konsultasi/index.php
@@ -1,7 +1,18 @@
 <!-- SESUIKAN UKURAN BOX MAIN -->
 
-<main class="m-6 p-8 flex-1 bg-[#ffffff] rounded-3xl shadow-lg/25">
+<main class="m-6 p-8 flex-1 bg-[#ffffff] place-self-center rounded-3xl shadow-lg/25 sm:w-auto md:w-md xl:w-3xl">
   <div>
-    <h1 class="text-2xl font-bold">Konsultasi</h1>
+    <h1 class="text-2xl font-bold place-self-center">Konsultasi</h1>
+    <div class="container flex flex-col items-center justify-center mt-6">
+
+      <div class="hover:bg-[#d1dfff] kirim-pesan p-15 bg-[#bbcde4] m-10 rounded-3xl shadow-lg/25 w-2xs text-center text-xl">
+        <button class="font-bold">Kirim Pesan</button>
+      </div>
+      
+      <div class="hover:bg-[#d1dfff] lihat-pesan p-15 bg-[#bbcde4] m-10 rounded-3xl shadow-lg/25 w-2xs text-center text-xl">
+        <button class="font-bold">Lihat Pesan</button>
+      </div>
+    
+    </div>
   </div>
 </main>
--- app/views/siswa/konsultasi/lihat_pesan.php
+++ app/views/siswa/konsultasi/lihat_pesan.php
@@ -2,4 +2,49 @@
   <div>
     <h1 class="text-2xl font-bold">Pesan</h1>
   </div>
+
+  <!-- 1 Pesan -->
+  <div class="grid grid-cols-1 gap-6">
+    <div class="bg-[#3e70b2] rounded-lg grid grid-cols-2 shadow-md p-4 m-6">
+      <span class="text-left text-white font-semibold p-4">Topik Pesan</span>
+
+      <div class="col-span-2 p-4">
+        <div class="bg-[#ffffff] rounded-lg p-4 ">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, corporis totam! Molestias culpa assumenda minus
+          nulla placeat ad asperiores totam alias, sunt aliquid quis tempora vitae veniam provident quam doloremque?
+        </div>
+      </div>
+
+      <!-- Ini Div untuk Balasan -->
+      <div class="col-span-2 p-4">
+        <div class="bg-[#ffffff] col-span-2 rounded-lg p-4 ">
+          Ini kolom balasan, jadi jangan pilih salah satu antara balasan dan Tombol balas pesan! Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Qui, corporis totam! Molestias culpa assumenda minus nulla placeat ad
+          asperiores totam alias, sunt aliquid quis tempora vitae veniam provident quam doloremque?
+        </div>
+      </div>
+    </div>
+  
+    <!-- 1 Pesan Belum ada balasan -->
+  <div class="grid grid-cols-1 gap-6">
+    <div class="bg-[#3e70b2] rounded-lg grid grid-cols-2 shadow-md p-4 m-6">
+      <span class="text-left text-white font-semibold p-4">Topik Pesan</span>
+
+      <div class="col-span-2 p-4">
+        <div class="bg-[#ffffff] rounded-lg p-4 ">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, corporis totam! Molestias culpa assumenda minus
+          nulla placeat ad asperiores totam alias, sunt aliquid quis tempora vitae veniam provident quam doloremque?
+        </div>
+      </div>
+
+      <!-- Ini Div untuk Balasan -->
+      <div class="col-span-2 p-4">
+        <div class="bg-[#a76d18] col-span-2 rounded-lg p-4 text-center text-white">
+          Belum ada balasan
+        </div>
+      </div>
+    </div>
+
+
+
 </main>
