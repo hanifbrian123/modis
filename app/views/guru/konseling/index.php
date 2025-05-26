fa22a252463cd82aa1c7910fe08374b8445341d5
M Rizal Febrianto <ebiebi525@gmail.com>
2025-05-26 12:17:17 +0700

    Bismillah konseling done (kurang akses linknya)

--- app/views/guru/konseling/index.php
+++ app/views/guru/konseling/index.php
@@ -1,5 +1,35 @@
-<main class="m-6 p-8 flex-1 bg-[#ffffff] rounded-3xl shadow-lg/25">
+<main class="m-6 flex-1 bg-[#ffffff] rounded-3xl shadow-lg/25">
   <div>
-    <h1 class="text-2xl font-bold">Daftar Pesan</h1>
+    <h1 class="text-2xl font-bold ms-5 mt-5">Daftar Pesan</h1>
+  </div>
+
+  <div class="grid grid-cols-1 gap-6">
+    <div class="bg-[#3e70b2] rounded-lg grid grid-cols-2 shadow-md p-4 m-6">
+      <span class="text-left text-white font-semibold p-4">sdad</span>
+      <span class="text-right text-white font-semibold p-4">asdsa</span>
+
+      <div class="col-span-2 p-4">
+        <div class="bg-[#ffffff] rounded-lg p-4 ">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, corporis totam! Molestias culpa assumenda minus
+          nulla placeat ad asperiores totam alias, sunt aliquid quis tempora vitae veniam provident quam doloremque?
+        </div>
+      </div>
+
+      <!-- Ini Div untuk Balasan -->
+      <div class="col-span-2 p-4">
+        <div class="bg-[#ffffff] col-span-2 rounded-lg p-4 ">
+          Ini kolom balasan, jadi jangan pilih salah satu antara balasan dan Tombol balas pesan! Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Qui, corporis totam! Molestias culpa assumenda minus nulla placeat ad
+          asperiores totam alias, sunt aliquid quis tempora vitae veniam provident quam doloremque?
+        </div>
+      </div>
+
+      <!-- Ini Div untuk Tombol Balasan -->
+      <div class="justify-self-center col-span-2 p-4">
+        <button class="bg-[#f4a024] hover:bg-[#a76d18] text-white px-10 py-2 rounded">Balas Pesan</button>
+      </div>
+
+    </div>
+
   </div>
 </main>
