eaab3a5a4dbba112bef8c7842bd085602c4ddaef
hanifbrian123 <brianhanif12345@gmail.com>
2025-06-06 09:18:35 +0700

    perbaikan sedikit UI

--- app/views/siswa/lapor_bk/index.php
+++ app/views/siswa/lapor_bk/index.php
@@ -1,14 +1,14 @@
 <!-- SESUAIKAN UKURAN BOX MAIN -->
 <main class="m-6 p-8 flex-1 bg-[#ffffff] rounded-3xl shadow-lg/25">
   <div>
-    <h1 class="text-2xl font-bold text-center mb-8">Laporan Kepada BK</h1>
+    <h1 class="text-2xl font-bold text-center mb-8">Laporankan Kepada BK</h1>
   </div>
 
   <form action="<?= BASEURL ?>/siswa/simpan_laporan" method="POST" enctype="multipart/form-data">
     <!-- Nama Pelanggar -->
     <div class="mb-6">
       <label class="block font-semibold mb-2">Nama Pelanggar <span class="text-red-500">*</span></label>
-      <select name="nis_terlapor" class="w-full bg-blue-200 text-black px-4 py-3 rounded-full shadow focus:outline-none focus:ring-2 focus:ring-blue-500">
+      <select name="nis_terlapor" class="w-full bg-[#bbcde4] text-black px-4 py-3 rounded-full shadow focus:outline-none focus:ring-2 focus:ring-blue-500">
         <option value="">Pilih nama siswa</option>
         <?php foreach ($data['siswa'] as $s): ?>
           <option value="<?= $s['nis'] ?>"><?= $s['nama'] ?></option>
@@ -19,7 +19,7 @@
     <!-- Jenis Pelanggaran -->
     <div class="mb-6">
       <label class="block font-semibold mb-2">Pilih Jenis Pelanggaran</label>
-      <select name="jenis" class="w-full bg-blue-200 text-black px-4 py-3 rounded-full shadow focus:outline-none focus:ring-2 focus:ring-blue-500">
+      <select name="jenis" class="w-full bg-[#bbcde4] text-black px-4 py-3 rounded-full shadow focus:outline-none focus:ring-2 focus:ring-blue-500">
         <?php  foreach ($data['pelanggaran'] as $p): ?>
           <option value="<?= $p['ID'] ?>"><?= $p['NamaPelanggaran'] ?></option>
         <?php endforeach; ?>
@@ -29,13 +29,19 @@
     <!-- Bukti Gambar -->
     <div class="mb-6">
       <label class="block font-semibold mb-2">Bukti Gambar</label>
-      <input type="file" name="bukti" accept="image/*" class="block w-full text-sm text-black bg-blue-200 rounded-full px-4 py-2 shadow">
+      <input type="file" name="bukti" accept="image/*" class="
+              block w-full text-sm text-slate-500
+              file:mr-4 file:py-2 file:px-4
+              file:rounded-full file:border-0
+              file:text-sm file:font-semibold
+              file:bg-[#bbcde4] file:text-black
+              hover:file:bg-blue-300 transition">
     </div>
 
     <!-- Deskripsi -->
     <div class="mb-6">
       <label class="block font-semibold mb-2">Deskripsi Pelanggaran</label>
-      <textarea name="deskripsi" rows="5" class="w-full bg-blue-200 text-black px-4 py-3 rounded-2xl shadow focus:outline-none focus:ring-2 focus:ring-blue-500 resize-none"></textarea>
+      <textarea name="deskripsi" rows="5" class="w-full bg-[#bbcde4] text-black px-4 py-3 rounded-2xl shadow focus:outline-none focus:ring-2 focus:ring-blue-500 resize-none" placeholder="Tambahkan Deskripsi Pelanggaran"></textarea>
     </div>
 
     <!-- Tombol Submit -->
